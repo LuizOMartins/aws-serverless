067c19df0a3b3d9c6ecb70d203526334c196cd16
LuizOMartinsProver <loliveira@provertec.com.br>
2024-03-23 21:47:11 -0300

    Initial Commit

---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+
+//scope: onde a Stack esta inserida
+
+export class ProductAppStack extends cdk.Stack {
+  readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/productsFetchFunction.ts",
+        handler: "handler",
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+      }
+    );
+  }
+}
